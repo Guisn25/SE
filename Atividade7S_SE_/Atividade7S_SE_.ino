288d73e2c323c477a45d74f165ca69c5d20de56e
Guisn25 <127779512+Guisn25@users.noreply.github.com>
2024-12-11 03:22:03 -0300

    Update Atividade7S_SE_.ino

--- Atividade7S_SE_/Atividade7S_SE_.ino
+++ Atividade7S_SE_/Atividade7S_SE_.ino
@@ -1,10 +1,5 @@
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-const int BotaoBACK = 10;
-const int BotaoENTER = 9;
-const int BotaoDOWN = 8;
-const int BotaoUP = 7;
 
 int estadoBotaoBACK = 0;
 int estadoBotaoENTER = 0;
@@ -13,9 +8,6 @@ int estadoBotaoUP = 0;
 
 int estadoBotao;
 unsigned long tempoDebounce = 0;
-unsigned long debounceDelay = 200;
-
-unsigned long tempo;
 
 int debouncedRead(int Botao){  
   int estadoBotaoUltimo = LOW;
@@ -29,42 +21,39 @@ int debouncedRead(int Botao){
       estadoBotao = leitura;
     }
     estadoBotaoUltimo = estadoBotao;
-  }while((millis() - tempoDebounce) < debounceDelay);
+  }while((millis() - tempoDebounce) < 50);
   
   return estadoBotao;
 }
 
 void setup(){
   lcd.begin(16, 2);
-  pinMode(estadoBotaoBACK, INPUT);
-  pinMode(estadoBotaoENTER, INPUT);
-  pinMode(estadoBotaoDOWN, INPUT);
-  pinMode(estadoBotaoUP, INPUT);
+  pinMode(10, INPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+  pinMode(7, INPUT);
 }
 
 int i=1;
 int j;
 void loop(){
-  estadoBotaoENTER = debouncedRead(BotaoENTER);
-  estadoBotaoDOWN = debouncedRead(BotaoDOWN);
-  estadoBotaoUP = debouncedRead(BotaoUP);
+  estadoBotaoENTER = debouncedRead(9);
+  estadoBotaoDOWN = debouncedRead(8);
+  estadoBotaoUP = debouncedRead(7);
 
   if(estadoBotaoENTER == LOW){
     lcd.setCursor(0,0);
     lcd.print("Opcao ");
     lcd.print(i);
     lcd.print("*");
-    j = (i+1)%6;
-    if(j == 0){
-      j=1;
-    }
+    j = (i%5)+1;
     lcd.setCursor(0,1);
     lcd.print("Opcao ");
     lcd.print(j);
   }else{
     lcd.clear();
     do{
-      estadoBotaoBACK = debouncedRead(BotaoBACK);
+      estadoBotaoBACK = debouncedRead(10);
       lcd.setCursor(0,0);
       lcd.print("Escolhi a opcao ");
       lcd.setCursor(0,1);
@@ -73,17 +62,11 @@ void loop(){
     }while(estadoBotaoBACK == LOW);  
     lcd.clear();
   }
-//Essa parte ficou feia
+
   if(estadoBotaoUP == HIGH){
-    i = (i+1)%6;
+    i = (i%5)+1;
   }
   if(estadoBotaoDOWN == HIGH){
-    i = (i-1)%6;
-    if(i==0){
-      i=5;
-    }
-  }
-  if(i==0){
-    i=1;
+    i = ((i+3)%5)+1;
   }
 }
